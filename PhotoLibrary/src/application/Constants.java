365f4fedca92e88d520eb8ee6a1727a860288c94
Johannes Priebsch <johannes@priebsch.at>
2020-11-16 08:38:27 +0100

    move fxml and css to view-package

--- PhotoLibrary/src/application/Constants.java
+++ PhotoLibrary/src/application/Constants.java
@@ -4,10 +4,10 @@ package application;
 public class Constants {
 
 	// fxml resources
-	public static final String PATH_TO_SLIDESHOW_VIEW_FXML = "/fxml/SlideshowView.fxml";
-	public static final String PATH_TO_PHOTO_LIBRARY_VIEW_FXML = "/fxml/PhotoLibraryView.fxml";
-	public static final String PATH_TO_PHOTOGRAPHER_VIEW_FXML = "/fxml/PhotographerView.fxml";
-	public static final String PATH_TO_TAB_VIEW_FXML = "/fxml/TabView.fxml";
+	public static final String PATH_TO_SLIDESHOW_VIEW_FXML = "/view/fxml/SlideshowView.fxml";
+	public static final String PATH_TO_PHOTO_LIBRARY_VIEW_FXML = "/view/fxml/PhotoLibraryView.fxml";
+	public static final String PATH_TO_PHOTOGRAPHER_VIEW_FXML = "/view/fxml/PhotographerView.fxml";
+	public static final String PATH_TO_TAB_VIEW_FXML = "/view/fxml/TabView.fxml";
 	
 	
 }
